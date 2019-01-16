8428d2b27ac1d143393681abb5bc8fc644d0738b
Maxime Gréau <maxime.greau@elastic.co>
2019-01-16 13:27:11 +0100

    Init tutorials

---
+++ pipeline-elastic/scripts/set-up-env.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+[[ -n $DEBUG ]] && set -o xtrace
+
+readonly GIT_TOPLEVEL=$(git rev-parse --show-toplevel 2> /dev/null)
+
+# Donwload the Elastic Docker images used in the cluster
+TAG=6.5.4
+docker pull docker.elastic.co/elasticsearch/elasticsearch:${TAG}
+docker pull docker.elastic.co/kibana/kibana:${TAG}
+docker pull docker.elastic.co/beats/filebeat:${TAG}
+docker pull docker.elastic.co/beats/metricbeat:${TAG}
+
+# Deploy the all the components needed
+INIT="$1"
+if [[ -z "$INIT" ]]
+then
+	kubectl delete -f "${GIT_TOPLEVEL}/pipeline-elastic/config"
+  kubectl delete namespace tutorials
+fi
+kubectl apply -f "${GIT_TOPLEVEL}/pipeline-elastic/config"
+
+# Create the namespace where the pods will be deployed
+kubectl create namespace tutorials
+
