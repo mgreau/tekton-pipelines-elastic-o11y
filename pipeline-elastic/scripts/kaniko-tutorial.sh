c7bfc52782f553232260204dbf8ea7520bae2378
Chris Koehnke <chris.koehnke@elastic.co>
2019-02-07 09:42:38 -0700

    Allow docker hub username to be passed in

--- pipeline-elastic/scripts/kaniko-tutorial.sh
+++ pipeline-elastic/scripts/kaniko-tutorial.sh
@@ -3,6 +3,12 @@ set -e
 
 [[ -n $DEBUG ]] && set -o xtrace
 
+if [[ -z "$DOCKER_HUB_USERNAME" ]]
+then
+  echo 'The environment variable DOCKER_HUB_USERNAME must be set.'
+  exit 1
+fi
+
 readonly GIT_TOPLEVEL=$(git rev-parse --show-toplevel 2> /dev/null)
 readonly NAMESPACE=kaniko-tutorial
 
@@ -14,6 +20,8 @@ fi
 
 kubectl create secret generic kaniko-secret --from-file="config.json" -n "$NAMESPACE"
 
-kubectl apply -f "${GIT_TOPLEVEL}/pipeline-elastic/samples/02-kaniko-push-dockerhub"
+sed -e "s/{{DOCKER_HUB_USERNAME}}/$DOCKER_HUB_USERNAME/" \
+  "${GIT_TOPLEVEL}"/pipeline-elastic/samples/02-kaniko-push-dockerhub/* \
+  | kubectl apply -f -
 
 
