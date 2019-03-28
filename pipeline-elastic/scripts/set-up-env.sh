2170b9a79617e0d0ebf7461c227ab1edf6f1bd04
Maxime Gréau <maxime.greau@elastic.co>
2019-03-27 23:38:41 -0400

    Tekton Pipelines 0.2 + fix samples

--- pipeline-elastic/scripts/set-up-env.sh
+++ pipeline-elastic/scripts/set-up-env.sh
@@ -6,21 +6,16 @@ set -e
 readonly GIT_TOPLEVEL=$(git rev-parse --show-toplevel 2> /dev/null)
 
 # Donwload the Elastic Docker images used in the cluster
-TAG=6.6.0
+TAG=6.7.0
 docker pull docker.elastic.co/elasticsearch/elasticsearch:${TAG}
 docker pull docker.elastic.co/kibana/kibana:${TAG}
 docker pull docker.elastic.co/beats/filebeat:${TAG}
 docker pull docker.elastic.co/beats/metricbeat:${TAG}
 
-# Deploy the all the components needed
-INIT="$1"
-if [[ -z "$INIT" ]]
-then
-	kubectl delete -f "${GIT_TOPLEVEL}/pipeline-elastic/config"
-  kubectl delete namespace tutorials
-fi
+# Create the namespace where the pods will be deployed
+kubectl create namespace elastic-stack
+kubectl create namespace tutorials
 kubectl apply -f "${GIT_TOPLEVEL}/pipeline-elastic/config"
 
-# Create the namespace where the pods will be deployed
-kubectl create namespace tutorials
+
 
